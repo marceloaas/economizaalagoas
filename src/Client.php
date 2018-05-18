32dfca904c35aa46f7640a46b5d13a0ddaf5745b
Marcelo de Andrade <andrade.asmarcelo@gmail.com>
2018-05-18 14:56:40 -0300

    Adicione Guzzle e propriedades

--- src/Client.php
+++ src/Client.php
@@ -2,29 +2,71 @@
 
 /**
  * Cliente PHP para acesso à API do projeto Economiza Alagoas
- * 
+ *
  * PHP Version 7
- * 
+ *
  * @category API
  * @package  Economizaalagoas
  * @author   Marcelo de Andrade <andrade.asmarcelo@gmail.com>
  * @license  https://opensource.org/licenses/mit MIT
- * @link     https://github.com/marcelodeandrade/economizaalagoas-api-php-client 
+ * @link     https://github.com/marcelodeandrade/economizaalagoas-api-php-client
+ * Github
  */
 
 namespace Economizaalagoas;
 
+use GuzzleHttp\Client as GuzzleClient;
+
 /**
- * Cliente para acesso à API do projeto Economiza Alagoas 
- * 
+ * Cliente para acesso à API do projeto Economiza Alagoas
+ *
  * @category API
  * @package  Economizaalagoas
  * @author   Marcelo de Andrade <andrade.asmarcelo@gmail.com>
  * @license  https://opensource.org/licenses/mit MIT
- * @link     https://github.com/marcelodeandrade/economizaalagoas-api-php-client 
+ * @link     https://github.com/marcelodeandrade/economizaalagoas-api-php-client
  * Github
  */
 class Client
 {
+    /**
+     * Token de acesso
+     *
+     * @var [string]
+     */
+    protected $token;
 
+    /**
+     * URL base da API
+     *
+     * @var [string]
+     */
+    protected $baseUri = 'http://api.sefaz.al.gov.br';
+    
+    /**
+     * Instância de GuzzleHttp\Client
+     *
+     * @var [\GuzzleHttp\Client]
+     */
+    protected $httpClient;
+
+    /**
+     * __construct
+     *
+     * @param string $token
+     */
+    public function __construct(string $token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Retorna a instância de \GuzzleHttp\Client
+     *
+     * @return \GuzzleHttp\Client
+     */
+    public function getHttpClient(): \GuzzleHttp\Client
+    {
+        return $this->httpClient ?? $this->httpClient = new GuzzleClient();
+    }
 }
