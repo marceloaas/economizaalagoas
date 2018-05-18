e9aac95acfa4604d7e98876742b284cd9eb8f67d
Marcelo de Andrade <andrade.asmarcelo@gmail.com>
2018-05-18 10:23:09 -0300

    Adicione classe Client

---
+++ src/Client.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * Cliente PHP para acesso à API do projeto Economiza Alagoas
+ * 
+ * PHP Version 7
+ * 
+ * @category API
+ * @package  Economizaalagoas
+ * @author   Marcelo de Andrade <andrade.asmarcelo@gmail.com>
+ * @license  https://opensource.org/licenses/mit MIT
+ * @link     https://github.com/marcelodeandrade/economizaalagoas-api-php-client 
+ */
+
+namespace Economizaalagoas;
+
+/**
+ * Cliente para acesso à API do projeto Economiza Alagoas 
+ * 
+ * @category API
+ * @package  Economizaalagoas
+ * @author   Marcelo de Andrade <andrade.asmarcelo@gmail.com>
+ * @license  https://opensource.org/licenses/mit MIT
+ * @link     https://github.com/marcelodeandrade/economizaalagoas-api-php-client 
+ * Github
+ */
+class Client
+{
+
+}
